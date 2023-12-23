6f15cfb7fa253d05224fda26769eebfac96e9dd1
Pedro Silva <pedro.silva-dev@hotmail.com>
2023-12-22 22:59:15 -0300

    feat: center firebase config object

---
+++ src/app/modules/@core/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  projectId: `iap-base`,
+  messagingSenderId: `1066573489407`,
+  storageBucket: `iap-base.appspot.com`,
+  authDomain: `iap-base.firebaseapp.com`,
+  apiKey: `AIzaSyAk4hO9FnWZDaYXNUTQgm6ScU3fATgfRhc`,
+  appId: `1:1066573489407:web:39b9037a7b1c14aefad8ca`,
+};
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,11 +1,6 @@
+import { FIREBASE_CONFIG } from '../app/modules/@core/firebase/firebase.config';
+
 export const environment = {
   production: false,
-  firebase: {
-    projectId: `iap-base`,
-    messagingSenderId: `1066573489407`,
-    storageBucket: `iap-base.appspot.com`,
-    authDomain: `iap-base.firebaseapp.com`,
-    apiKey: `AIzaSyAk4hO9FnWZDaYXNUTQgm6ScU3fATgfRhc`,
-    appId: `1:1066573489407:web:39b9037a7b1c14aefad8ca`,
-  },
+  firebase: FIREBASE_CONFIG,
 };
