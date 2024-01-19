09729adbebd4f6260dfbcd4f176213d86b4cee04
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-01-19 20:03:54 -0300

    feat: init event detail

--- src/app/modules/portal/components/portal-card/portal-card.component.ts
+++ src/app/modules/portal/components/portal-card/portal-card.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AnimateDirective } from '../../../@core/directives/animate.directive';
 
 @Component({
   standalone: true,
   selector: 'portal-card',
-  imports: [AnimateDirective],
   styleUrl: './portal-card.component.scss',
+  imports: [AnimateDirective, RouterModule],
   templateUrl: './portal-card.component.html',
 })
 export class PortalCardComponent {
   @Input() badge: string | null = '';
+  @Input() route: string | null = '';
   @Input({ required: true }) title: string = '';
   @Input({ required: true }) background: string = '';
   @Input({ required: true }) description: string = '';
--- src/app/modules/portal/pages/event/event-detail/event-detail.component.ts
+++ src/app/modules/portal/pages/event/event-detail/event-detail.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation,
+  inject,
+  signal,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingStore } from '../../../../../store/loading.store';
+import { AlertService } from '../../../../@core/services/alert.service';
+import {
+  IEventDB,
+  IEventItem,
+} from '../../../../@shared/interface/event.interface';
+import { DatabaseService } from '../../../../@shared/services/database.service';
 
 @Component({
-  selector: 'app-event-detail',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
+  selector: 'portal-event-detail',
+  styleUrl: './event-detail.component.scss',
   templateUrl: './event-detail.component.html',
-  styleUrl: './event-detail.component.scss'
+  encapsulation: ViewEncapsulation.None,
 })
 export class EventDetailComponent {
+  @ViewChild('mapContainer', { static: true }) mapContainer:
+    | ElementRef
+    | undefined;
 
+  public eventId = signal('');
+  public event: IEventItem = {} as IEventItem;
+
+  public loadingStore = inject(LoadingStore);
+
+  constructor(
+    private renderer: Renderer2,
+    private alertService: AlertService,
+    private activatedRoute: ActivatedRoute,
+    private databaseService: DatabaseService
+  ) {}
+
+  ngOnInit() {
+    this.eventId.set(this.activatedRoute.snapshot.params['id']);
+    this.getEvent();
+  }
+
+  public getEvent() {
+    this.loadingStore.setState(true);
+    this.databaseService.event
+      .getById<IEventDB>(this.eventId())
+      .then((response) => {
+        this.event = this.databaseService._model.event.buildItem(response);
+
+        if (this.mapContainer) {
+          this.renderer.setProperty(
+            this.mapContainer.nativeElement,
+            'innerHTML',
+            this.event.addressMapHTML
+          );
+        }
+        console.log('GET EVENT', this.event);
+      })
+      .catch((error) => this.alertService.snackDefaultResponseError(error))
+      .finally(() => this.loadingStore.setState(false));
+  }
 }
