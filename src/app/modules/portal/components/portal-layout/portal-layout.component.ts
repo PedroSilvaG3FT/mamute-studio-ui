cbbb0b5c1c47b9c971b5a4b25e3245f2b38da3ff
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-02-23 20:25:36 -0300

    feat: whatsapp button

--- src/app/modules/portal/components/portal-layout/portal-layout.component.ts
+++ src/app/modules/portal/components/portal-layout/portal-layout.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WhatsAppService } from '../../../@core/services/whatsapp.service';
 import { PortalFooterComponent } from '../portal-footer/portal-footer.component';
 import { PortalHeaderComponent } from '../portal-header/portal-header.component';
 import { PortalUserMenuComponent } from '../portal-user-menu/portal-user-menu.component';
@@ -16,4 +17,10 @@ import { PortalUserMenuComponent } from '../portal-user-menu/portal-user-menu.co
     PortalUserMenuComponent,
   ],
 })
-export class PortalLayoutComponent {}
+export class PortalLayoutComponent {
+  public whatsAppService = inject(WhatsAppService);
+
+  public handleOpenWhatsapp() {
+    this.whatsAppService.sendMessage();
+  }
+}
